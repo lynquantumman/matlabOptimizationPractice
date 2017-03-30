b9d582b0c173a1e30637a77d82d5c6662bc7fb94
lynquantumman <q2511757@126.com>
2017-03-30 14:54:02 +0800

    no message

--- example2_10.m
+++ example2_10.m
@@ -2,3 +2,11 @@ U = sym('3*x^2+5*y+2*x*y+6');
 syms x y;
 V = 3*x^2+5*y+2*x*y+6;
 disp(2*U-V-6);
+%==============
+symbols_number = sym('[1,2]');
+disp(symbols_number);
+
+%符号矩阵，以及符号矩阵求行列式
+matrix_symbol = sym('[a,b;c,d]');
+deter_symbol = det(matrix_symbol);
+disp(deter_symbol);
